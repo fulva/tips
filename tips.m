5ad90c195ba0a273befec49022814c4ba6470aa3
fulva <fulva.hyh@gmail.com>
2020-04-28 23:21:07 +0800

    tips.m

---
+++ tips.m
@@ -0,0 +1,16 @@
+# gpg: 无法检查签名:找不到公钥
+
+```
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 76F1A20FF987672F
+```
+
+# http server
+```
+git config --global http.proxy http://127.0.0.1:8001
+
+git config --global https.proxy https://127.0.0.1:1080
+
+git config --global --unset http.proxy
+
+git config --global --unset https.proxy
+```
